393fcd4aadd9b1f15645f96d166f2305367832bf
Eduardo <edusartori00@gmail.com>
2023-01-16 15:57:15 -0300

    Test com outro Banco de Dados

--- src/test/java/br/com/springboot/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/springboot/forum/repository/CursoRepositoryTest.java
@@ -5,23 +5,43 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class CursoRepositoryTest {
 
     @Autowired
     private CursoRepository repository;
+    @Autowired
+    private TestEntityManager manager;
 
     //O teste deve carregar um curso ao buscar por seu nome!
     @Test
     public void test(){
         String nomeCurso = "HTML 5";
+
+        Curso html5 = new Curso();
+        html5.setNome(nomeCurso);
+        html5.setCategoria("Programacao");
+        manager.persist(html5);
+
         Curso curso = repository.findByNome(nomeCurso);
         Assert.assertNotNull(curso);
         Assert.assertEquals(nomeCurso, curso.getNome());
+    }
+
+    //Não deve carregar o curso onde o nome não esta cadastrado.
+    public void test2() {
+        String nome = "JPA";
+        Curso curso = repository.findByNome(nome);
+        Assert.assertNull(curso);
     }
 
 }
